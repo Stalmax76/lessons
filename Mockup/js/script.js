c62f3280eef05cc0eff5bdebe52e7bafe050cf1e
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-01-25 10:24:05 +0100

    write changes in Mockup

---
+++ Mockup/js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const button = document.getElementById('actionButton');
+//   const status = document.getElementById('status');
+
+//   button.addEventListener('click', () => {
+//     status.textContent = 'Кнопку натиснуто!';
+//   });
+// });
+
+//==========
